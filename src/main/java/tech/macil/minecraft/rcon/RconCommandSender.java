8060ce9602bcacc24d4fb753d58aaee181c04713
Chris Cowan <agentme49@gmail.com>
2017-11-13 21:27:39 -0800

    Run "version" command.

---
+++ src/main/java/tech/macil/minecraft/rcon/RconCommandSender.java
@@ -0,0 +1,106 @@
+package tech.macil.minecraft.rcon;
+
+import org.bukkit.Server;
+import org.bukkit.command.CommandSender;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionAttachment;
+import org.bukkit.permissions.PermissionAttachmentInfo;
+import org.bukkit.plugin.Plugin;
+
+import java.util.Set;
+import java.util.logging.Level;
+
+class RconCommandSender implements CommandSender {
+    private final RconPlugin plugin;
+
+    RconCommandSender(RconPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void sendMessage(String message) {
+        plugin.getLogger().log(Level.INFO, "Response: " + message);
+    }
+
+    @Override
+    public void sendMessage(String[] messages) {
+        for (String message : messages) {
+            sendMessage(message);
+        }
+    }
+
+    @Override
+    public Server getServer() {
+        return plugin.getServer();
+    }
+
+    @Override
+    public String getName() {
+        return "$RconPlugin";
+    }
+
+    @Override
+    public boolean isPermissionSet(String name) {
+        return true;
+    }
+
+    @Override
+    public boolean isPermissionSet(Permission perm) {
+        return true;
+    }
+
+    @Override
+    public boolean hasPermission(String name) {
+        return true;
+    }
+
+    @Override
+    public boolean hasPermission(Permission perm) {
+        return true;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, String name, boolean value, int ticks) {
+        return null;
+    }
+
+    @Override
+    public PermissionAttachment addAttachment(Plugin plugin, int ticks) {
+        return null;
+    }
+
+    @Override
+    public void removeAttachment(PermissionAttachment attachment) {
+
+    }
+
+    @Override
+    public void recalculatePermissions() {
+
+    }
+
+    @Override
+    public Set<PermissionAttachmentInfo> getEffectivePermissions() {
+        return null;
+    }
+
+    @Override
+    public boolean isOp() {
+        return true;
+    }
+
+    @Override
+    public void setOp(boolean value) {
+
+    }
+}
--- src/main/java/tech/macil/minecraft/rcon/RconPlugin.java
+++ src/main/java/tech/macil/minecraft/rcon/RconPlugin.java
@@ -1,5 +1,7 @@
 package tech.macil.minecraft.rcon;
 
+import org.bukkit.command.CommandException;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Level;
@@ -7,7 +9,20 @@ import java.util.logging.Level;
 public class RconPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
+        saveDefaultConfig();
         getLogger().log(Level.INFO, "onEnable");
+
+        String listenAddress = getConfig().getString("listenAddress");
+        int port = getConfig().getInt("port");
+
+        CommandSender sender = new RconCommandSender(this);
+        try {
+            if (!getServer().dispatchCommand(sender, "version")) {
+                getLogger().log(Level.SEVERE, "Command not found");
+            }
+        } catch (CommandException e) {
+            getLogger().log(Level.SEVERE, "Error running command", e);
+        }
     }
 
     @Override
